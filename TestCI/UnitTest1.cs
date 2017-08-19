2212d980296854ce3098bcc9de3b93d4e7b637c3
ANDREW-PC\User <vnuk.andrew@gmail.com>
2017-08-19 15:23:12 +0300

    updating of nuget

--- TestCI/UnitTest1.cs
+++ TestCI/UnitTest1.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Threading;
 
 namespace TestCI
 {
